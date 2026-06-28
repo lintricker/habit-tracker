3e74d6b992c94a0fbe75ad7791fa0451c0bad9a4
lintricker <alina.ishem01@gmail.com>
2026-06-28 17:05:26 +0400

    feat/dashboard page

--- src/pages/DashboardPages.tsx
+++ src/pages/DashboardPages.tsx
@@ -1,8 +1,13 @@
+import StatCard from "../components/StatCard"
 
 const DashboardPage = () => {
   
   return (
-    <h1 className="dark:text-white">Dashboard</h1>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <StatCard label="Привычек всего" value="5" icon="📊" />
+      <StatCard label="Выполнено сегодня" value="3" icon="✅" />
+      <StatCard label="Текущая серия" value="7" icon="🔥" />
+    </div>
   )
 }
 export default DashboardPage

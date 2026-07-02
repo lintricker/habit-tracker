3833895faefb8dd6949c762f75a0de7065a90392
lintricker <alina.ishem01@gmail.com>
2026-07-02 20:33:24 +0400

    feat/logout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,19 +1,40 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet, useNavigate } from "react-router-dom"
 import { ThemeToggle } from "./ThemeToggle"
 import { PiSignOutThin } from "react-icons/pi";
+import { supabase } from "../lib/supabase";
+import { useState } from "react";
 
 
 const Layout = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut()
+
+    if (error) {
+      alert(error.message)
+      return
+    }
+    
+    console.log("Signed out")
+    navigate('/login')
+  }
     return (
     <div className="min-h-screen bg-white dark:bg-zinc-900 text-zinc-900">
-      {/* Шапка */}
+      {}
       <header className="border-b border-zinc-300 dark:border-gray-700">
         <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
           <Link to="/" className="dark:text-white"><h1 className="text-xl font-bold dark:text-white">Habit Tracker</h1></Link>
           <nav className="flex gap-4 items-center">        
             <Link to="/habits" className="hover:text-blue-500 dark:text-white">Habits</Link>
             <Link to="/settings" className="hover:text-blue-500 dark:text-white">Settings</Link>
-            <Link to="/login" className="dark:text-white"><PiSignOutThin /></Link>
+            <button
+              onClick={handleLogout}
+              className="dark:text-white hover:text-blue-500 transition-colors"
+              title="Sign out"
+            >
+              <PiSignOutThin size={20} />
+            </button>
             <ThemeToggle />
           </nav>
         </div>
